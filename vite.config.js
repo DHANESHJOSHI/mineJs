2214f522f80eac134b180c6aaf2218910e6983a0
DHANESHJOSHI <hydradhanesh@gmail.com>
2024-04-23 00:53:50 +0530

    update serverConfig

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@
 * @type {import('vite').UserConfig}
 */
 export default {
-  base: '/minecraft-threejs-clone/',
+  base: '/',
   build: {
     sourcemap: true
-  }
+  },
+  server: {
+    host: '0.0.0.0', // Change this to your desired IP address
+  },
+
 }
